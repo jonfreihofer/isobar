a1887e9e6a3dbc9a796c02f1ddf17ec6c455c8e2
Jonathan Freihofer <patientfrei@gmail.com>
2021-10-28 21:11:34 -0400

    programmed addColorStop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import "./App.css";
-import { useRef, useEffect } from "react";
+import Isobar from "../src/components/Isobar";
 
 function App() {
-  const Isobar = (props) => {
-    const canvasRef = useRef(null);
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      let ctx = canvas.getContext("2d");
-      let gradient = ctx.createLinearGradient(20, 0, 220, 0);
-      // Add three color stops
-      gradient.addColorStop(0, "green");
-      gradient.addColorStop(0.5, "cyan");
-      gradient.addColorStop(1, "green");
-
-      // Set the fill style and draw a rectangle
-      ctx.fillStyle = gradient;
-      ctx.fillRect(20, 20, 200, 100);
-    }, []);
-    return <canvas ref={canvasRef} {...props} />;
-  };
   return (
     <div className="isobar">
       <Isobar />
